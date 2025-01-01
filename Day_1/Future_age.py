a3db285eaffa462ee22cb7e4aa1606089bc94109
root <root@DESKTOP-QJV8SET.>
2025-01-01 14:21:57 +0300

    Future age

---
+++ Day_1/Future_age.py
@@ -0,0 +1,9 @@
+# Ask for user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Calculate the future age
+future_age = age + 5 
+
+# Output the result
+print(f"Hello {name}! In 5 years, you will be {future_age} years old.")
