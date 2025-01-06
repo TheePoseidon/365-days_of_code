5fba5294203a5263e2d0adf2adee23ac0968be91
TheePoseidon <j.ngugigiat@alustudent.com>
2025-01-06 23:05:51 +0300

    Readme

---
+++ Day_3/README.md
@@ -0,0 +1,3 @@
+# Day 3 innit
+# 3rd Jan 2025
+# Just Do It
---
+++ Day_3/variables-data_type.py
@@ -0,0 +1,9 @@
+# User input
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+# Calculate future age
+age = int(age) + 10
+
+# Print the result
+print(f"Hello {name}, you will be {age} in 10 years.")
