90caa4beb1c158cb1525ec5fa1e2f5a474008987
TheePoseidon <j.ngugigiat@alustudent.com>
2025-01-10 20:42:07 +0300

    Simple script

--- Day_10/script.js
+++ Day_10/script.js
@@ -19,3 +19,25 @@ console.log("Updated Name:", name);
 
 // Trying to change a const (this will throw an error)
 // age = 26;
+// Strings
+let greeting = "Hello, World!";
+console.log(greeting);
+
+// Numbers
+let price = 99.99;
+console.log("Price:", price);
+
+// Arrays
+let colors = ["red", "blue", "green"];
+console.log("Colors:", colors);
+console.log("First Color:", colors[0]);
+
+// Objects
+let person = {
+    firstName: "James",
+    lastName: "Ngugi",
+    age: 25,
+    isDeveloper: true
+};
+console.log("Person:", person);
+console.log("First Name:", person.firstName);
