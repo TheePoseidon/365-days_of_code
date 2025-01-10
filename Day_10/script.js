6405bc4fb610e6a097176a116306154a2b018906
TheePoseidon <j.ngugigiat@alustudent.com>
2025-01-10 20:41:28 +0300

    Simple script

--- Day_10/script.js
+++ Day_10/script.js
@@ -3,3 +3,19 @@ console.log("Hello, JavaScript!");
 
 // Change the content of the paragraph with id "demo"
 document.getElementById("demo").innerText = "Hello, JavaScript is awesome!";
+// Declaring variables
+let name = "James"; // A string
+const age = 25; // A number
+var isLearning = true; // A boolean
+
+// Displaying values in the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Learning JavaScript:", isLearning);
+
+// Reassigning a variable declared with let
+name = "James Ngugi";
+console.log("Updated Name:", name);
+
+// Trying to change a const (this will throw an error)
+// age = 26;
