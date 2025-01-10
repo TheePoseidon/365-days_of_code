6856fa147cbd493699d6e4c7ba79f58f259587d6
TheePoseidon <j.ngugigiat@alustudent.com>
2025-01-10 20:39:12 +0300

    Simple script

---
+++ Day_10/script.js
@@ -0,0 +1,5 @@
+// Display a message in the console
+console.log("Hello, JavaScript!");
+
+// Change the content of the paragraph with id "demo"
+document.getElementById("demo").innerText = "Hello, JavaScript is awesome!";
