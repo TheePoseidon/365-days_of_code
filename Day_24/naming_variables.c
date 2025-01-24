5569b8d5e46133a7a2fe4fb05177de01720613a6
TheePoseidon <j.ngugigiat@alustudent.com>
2025-01-24 22:58:08 +0300

    Naming variables

---
+++ Day_24/naming_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    char grade = 'A';
+    float score = 98.5;
+
+    printf("Age: %d\n", age);
+    printf("Grade: %c\n", grade);
+    printf("Score: %.2f\n", score);
+    return 0;
+    }
