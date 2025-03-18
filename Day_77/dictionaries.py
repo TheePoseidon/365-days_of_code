7aff001ce26d47009f0397c1dc71d5516310f7d4
TheePoseidon <j.ngugigiat@alustudent.com>
2025-03-18 19:22:59 +0300

    Dictionaries

---
+++ Day_77/dictionaries.py
@@ -0,0 +1,10 @@
+capital_cities = {
+    "India": "New Delhi",
+    "USA": "Washington D.C.",
+    "UK": "London",
+    "Australia": "Canberra",
+    "Japan": "Tokyo"
+}
+
+print(capital_cities)
+print(capital_cities["UK"])
