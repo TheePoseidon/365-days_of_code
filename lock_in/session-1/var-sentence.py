eb3abcb3f4e37e1ba478698a463c77ba652708fc
TheePoseidon <j.ngugigiat@alustudent.com>
2026-02-28 22:51:41 +0200

    Variables that print a sentence

---
+++ lock_in/session-1/var-sentence.py
@@ -0,0 +1,5 @@
+name = input("My name is: ")
+age = input("Age: ")
+favorite_number = input("Favorite number: ")
+
+print(f"My name is {name} and I am {age} years old. My favorite number is {favorite_number}")
