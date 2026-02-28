57404d1746fcc762ddc29d95a7afa68083b18f77
TheePoseidon <j.ngugigiat@alustudent.com>
2026-02-28 23:05:16 +0200

    done

--- lock_in/session-1/get-age.py
+++ lock_in/session-1/get-age.py
@@ -1,3 +1,3 @@
 current_age = input("How old are you: ")
 
-print(f"In 2026 I am {current_age} years old. In 2031 I will be {current_age + 5"})
+print(f"In 2026 I am {current_age} years old. In 2031 I will be {current_age + 5}")
