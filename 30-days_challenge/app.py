2597b5db269888dca9b3361577ae9ad4c0eaa13b
TheePoseidon <j.ngugigiat@alustudent.com>
2026-04-28 13:16:04 +0200

    String concatenations & Interpolations

--- 30-days_challenge/app.py
+++ 30-days_challenge/app.py
@@ -2,3 +2,30 @@
 #last = "Ngugi"
 #print(f"My name is {first} {last}")
 
+# String Cocatenation
+
+my_str_1 = "Hello"
+my_str_2 = "World"
+
+# Using the + operator
+
+#str_plus_str = my_str_1 + " " + my_str_2
+#print(str_plus_str)
+
+# Using the augmented assignment operator
+
+name = "James"
+age = 30
+
+#name_and_age = name
+#name_and_age += " " + str(age)
+
+#print(name_and_age)
+
+# Fstrings
+
+#name = "James Ngugi"
+#age = 30
+
+#name_and_age = f"My name is {name} and I am {age} years old"
+#print(name_and_age)
