bc3033b75f2eada47f7dfc411082fafa42263e65
TheePoseidon <j.ngugigiat@alustudent.com>
2026-05-06 10:35:09 +0200

    declaring variables and compiling

--- c-programming/data.c
+++ c-programming/data.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     // declaring variables
-    
+    int student_id = 20;
+    float gpa = 3.5;
+    char grade = 'A';
+    double TGPA = 3.755;
+    long population = 1000000;
+
+    // printing the variables
+    printf("Student ID: %d\n", student_id);
+    printf("GPA: %.2f\n", gpa);
+    printf("Grade: %c\n", grade);
+    printf("TGPA: %.3lf\n", TGPA);
+    printf("Population: %ld\n", population);
 }
