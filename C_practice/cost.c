9ce9f34cf6c0c68a409057d00a4c4679af3e0947
TheePoseidon <j.ngugigiat@alustudent.com>
2026-06-14 15:31:31 +0200

    Cost calculator

---
+++ C_practice/cost.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int items = 80;
+	float cost_per_item = 24.60;
+	int items_purchased = 27;
+	float total_purchase_cost = cost_per_item * items_purchased;
+	float items_remaining = items - items_purchased;
+	char currency = '$';
+
+	printf("Number of items: %d\n", items);
+	printf("Cost each item: %.2f%c\n", cost_per_item, currency);
+	printf("Total cost: %.2f%c\n", total_purchase_cost, currency);
+	printf("Remaining items: %d\n", items_remaining);
+
+	return 0;
+}
