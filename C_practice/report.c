25e26087c0f6df7ba1543ea053cc70678854a12b
TheePoseidon <j.ngugigiat@alustudent.com>
2026-06-14 16:06:38 +0200

    Simple student report card

---
+++ C_practice/report.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int studentID = 16595;
+	float score = 87.9;
+	char grade = 'A';
+
+	printf("Student ID: %d\n", studentID);
+	printf("Student score: %.1f\n", score);
+	printf("Student grade: %c\n", grade);
+
+	return 0;
+}
