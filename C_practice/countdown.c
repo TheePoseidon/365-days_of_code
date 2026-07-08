a052bf6d4d0b16a390a4df5e10b846ee28244dbb
TheePoseidon <j.ngugigiat@alustudent.com>
2026-07-08 17:54:44 +0200

    countdown

---
+++ C_practice/countdown.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+	
+	 int countdown = 100;
+	 
+	 while (countdown > 0) {
+		 printf("%d\n", countdown);
+		 countdown--;
+	 }
+
+	 printf("Happy Birthday!!\n");
+
+	 return 0;
+}
